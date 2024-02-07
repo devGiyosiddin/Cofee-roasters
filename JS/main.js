c5011eb4dbeb3bb6408d3e3966f94df4f92ca4df
devGiyosiddin <devgiyosiddin@gmail.com>
2024-02-07 14:31:32 +0500

    Add fucntion on JS for closing Modal window on press key ESC

--- JS/main.js
+++ JS/main.js
@@ -4,5 +4,16 @@ const elButoon = document.querySelector('.order-summary_buttton');
 if (elButoon) {
     elButoon.addEventListener('click', function () {
          elModal.classList.add('os-modal--open')
-     })
+    })
+    function escRemove() {
+        if (elModal.classList.contains('os-modal--open')) {
+            elModal.classList.remove('os-modal--open')
+        }
+    };
+    document.addEventListener('keydown', function (event) {
+        // Check if the pressed key is 'Escape'
+        if (event.key === 'Escape') {
+            escRemove();
+        }
+    });
  }
